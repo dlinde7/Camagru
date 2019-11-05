aa428e2e6c369704c9709be8804a69259abecf4f
Danie LINDE <dlinde@c5r7s3.wethinkcode.co.za>
2019-11-05 10:44:04 +0200

    Login updated

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
+<?php
+include_once 'session.php';
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Camagru Home</title>
 </head>
 <body>
+
+    <?php if(!isset($_SESSION['username'])): ?>
     <a href="login.php">Login</a>
     <br>
     <a href="reg.php">Sign Up</a>
+    <?php else: ?>
+    <a href="logout.php">Logout</a>
+    <?php endif ?>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,3 +1,40 @@
+<?php
+include_once 'session.php';
+include_once 'config/database.php';
+
+try {
+    $dp = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+    $dp->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {}
+
+if (isset($_POST['login'])) {
+    $user = $_POST['user'];
+    $pwd = $_POST['pwd'];
+
+    $sqldt = "SELECT * FROM users WHERE username = :user";
+    $st = $dp->prepare($sqldt);
+    $st->execute(array(':user' => $user));
+
+    if($row = $st->fetch()){
+        $id = $row['id'];
+        $h_pwd = $row['password'];
+        $username = $row['username'];
+
+        if (password_verify($pwd, $h_pwd)) {
+            $_SESSION['id'] = $id;
+            $_SESSION['username'] = $username;
+            header("location: index.php");
+        }
+        else {
+            $result = "Username or Password was incorrect";
+        }
+    }
+    else {
+        $result = "Username or Password was incorrect";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,13 +43,14 @@
 <body>
     <a href="index.php">Home</a>
     <br>
+    <?php echo $result ?>
     <form method="post" action="">
     <table>
         <tr><td>Username</td>
-        <td><input type="tesxt" value="" placeholder="Enter Username" required></td></tr>
+        <td><input type="tesxt" value="" placeholder="Enter Username" required name="user"></td></tr>
         <tr><td>Password</td>
-        <td><input type="password" value="" placeholder="Enter Password" required></td></tr>
-        <tr><td></td><td><input type="submit" value="Login"></td></tr>
+        <td><input type="password" value="" placeholder="Enter Password" required name="pwd"></td></tr>
+        <tr><td></td><td><input type="submit" value="Login" name="login"></td></tr>
     </table>
     </form>
 </body>
